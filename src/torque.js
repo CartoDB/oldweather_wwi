89b92724f910b2bd27cb4f1abe8ce4774aecdd28
Simon Tokumine <si@tinypla.net>
2012-09-29 15:06:02 +0100

    make work on iOS

--- src/torque.js
+++ src/torque.js
@@ -1,3 +1,14 @@
+if(Function.prototype.bind == undefined){
+  Function.prototype.bind = function (bind) {
+      var self = this;
+      return function () {
+          var args = Array.prototype.slice.call(arguments);
+          return self.apply(bind || null, args);
+      };
+  };   
+}
+
+
 function Torque() {
   var args = Array.prototype.slice.call(arguments),
   callback = args.pop(),
